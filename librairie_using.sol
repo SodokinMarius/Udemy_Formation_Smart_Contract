94089f4b5700d7e7b147c5cc01c3743fc519db3b
SodokinMarius <yaomariussodokin@gmail.com>
2022-12-02 19:08:04 +0100

    feat : add librairie usage file

---
+++ librairie_using.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.8.17;
+
+import "https://github.com/SodokinMarius/Udemy_Formation_Smart_Contract/blob/main/Librairie.sol";
+
+contract Bank 
+{
+    mapping(address => uint) public accounts;
+
+    using SafeMath for int ;  //To define for whate you use the librairie
+
+    function deposit() public payable{
+        require(accounts[msg.sender]+msg.value >= accounts[msg.sender],"Overflow error");
+        int _amount = int(msg.value);
+         accounts[msg.sender] = accounts[msg.sender].add(_amount);
+    }
+
+    function withdraw(uint _amount) public {
+        require(_amount <= accounts[msg.sender],"The amount is not sufficient");
+        require(_amount>0,"Amount must be greater than 0");
+        accounts[msg.sender] = accounts[msg.sender].sub(_amount);
+
+    }
+
+    function getBallance ( address account) public view returns(uint)
+    {
+        return accounts[account];
+    }
+}
